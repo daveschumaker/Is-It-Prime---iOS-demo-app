3d06e8675c16984f0de9b4e04e3da18bad004ba4
Dave Schumaker <daveschumaker@gmail.com>
2016-12-12 21:49:07 -0800

    Initial commit

--- Is It Prime/ViewController.swift
+++ Is It Prime/ViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userNumber: UITextField!
+    @IBOutlet weak var primeResult: UILabel!
+    
+    var isPrime = true;
+    
+    @IBAction func checkPrimeButton(_ sender: Any) {
+        let getUserNumber = Int(userNumber.text!)
+        var startSearch = getUserNumber
+        startSearch -= startSearch
+        
+        while (startSearch > 0 && isPrime) {
+            print(startSearch)
+            
+            startSearch -= startSearch
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ README.md
@@ -0,0 +1,4 @@
+# Is It Prime?
+
+An iOS app written in Swift as part of a lesson for the following Udemy course:
+https://www.udemy.com/complete-ios-10-developer-course/learn/v4/overview
